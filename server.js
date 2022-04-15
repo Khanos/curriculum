ffa02955e4a07e27dfe5834542d9518e27e89b34
khanos <epilef.rodriguez@gmail.com>
2022-04-15 19:18:28 -0400

    Setting up a small express server to handle the heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000;
+  path = require('path'),
+  port = process.env.PORT || 80,
+  host = process.env.HOST || 'epilef-curriculum.herokuapp.com';
 
 const app = express();
 
 app.use(history());
-app.use(serveStatic(__dirname + '/dist/spa'));
-app.listen(port);
+app.use(serveStatic(path.join(__dirname + '/dist/spa')));
+app.listen(port, () => {
+  console.log('The app is running...');
+  console.log(`http://${host}:${port}`);
+});
+module.exports = server;
