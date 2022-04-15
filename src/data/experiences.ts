59c5bae31163258a82d6c8066491d4ff1df95074
khanos <epilef.rodriguez@gmail.com>
2022-04-15 19:24:34 -0400

    added data since now it's a private repo

---
+++ src/data/experiences.ts
@@ -0,0 +1,80 @@
+const experiences = [
+  {
+    title: 'Frontend Software Developer',
+    subtitle: '2021 - Present',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C4D0BAQHphD_i42zNrQ/company-logo_200_200/0/1633093485320?e=2147483647&v=beta&t=VnM9RYRQw7_ImnJ7Oc3tZwshdT6E80-HefgaIqJb8U4',
+    body: '<span aria-hidden="true"><b>Client:</b> <a href="https://www.linkedin.com/company/itpex/" target="_blank">ITPex Studio LLC.</a>. <b>Projects:</b> <a href="https://derfdice.com/" target="_blank">DerfDice</a> and a Proof of concept of an E-learning Platform with gamification. <b>Tech:</b> #VueJs #Vuetify #Vuex #Vue-Router #NodeJS #Express #JQuery #Bootstrap </span>',
+  },
+  {
+    title: 'Full-Stack Software Developer',
+    subtitle: '2020',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C560BAQG6V9KeOM-z4Q/company-logo_200_200/0/1519877807660?e=2147483647&v=beta&t=Z9wRRaVL9NAYsPTVM4HBLEuLvDCZs5oFPCq7549unhM',
+    body: '<span aria-hidden="true"><b>Client:</b> <a href="https://calvaryrobotics.com/" target="_blank">Calvary Robotics</a>. <b>Projects:</b> Dashboards, Order System. <b>Tech:</b> #DotNet #ActiveDirectory #JQuery #Bootstrap #SailsJs #NodeJS #Express #MicrosoftSQL </span>',
+  },
+  {
+    title: 'Full-Stack Software Developer',
+    subtitle: '2017',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C4E0BAQF-ZbiPO9PSXg/company-logo_200_200/0/1563201573969?e=2147483647&v=beta&t=-tqK_P3e8U40_BMB6d2tn30HUq2lM8e_2bUwUp9PSWU',
+    body: '<span aria-hidden="true"><b>Client:</b> <a href="https://www.lookatme-ventures.com/" target="_blank">LookAtMe Ventures LLC.</a>. <b>Projects:</b> DM2, Data-Collector, Vitral. <b>Tech:</b> #GoogleAPI #FacebookAPI #TwitterAPI #LinkedinAPI #JQPlot #JQuery #Bootstrap #SailsJs #NodeJS #Express #MicrosoftSQL</span>',
+  },
+  {
+    title: 'Full-Stack Software Developer',
+    subtitle: '2016',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1256814174/twitter-profile_400x400.png',
+    body: '<span aria-hidden="true"><b>Client:</b> <a href="http://www.mediatrax.com/" target="_blank">MediaTrax LLC.</a>. <b>Projects:</b> I-Boards, Google Adwords (Now Google Ads), Okta <b>Tech:</b> #Okta #Adwords #JQuery #Bootstrap #SailsJs #NodeJS #Express #MicrosoftSQL</span>',
+  },
+  {
+    title: 'Full-Stack Software Developer',
+    subtitle: '2016',
+    avatar:
+      'https://pbs.twimg.com/profile_images/664822083847741440/arpG-xkO_400x400.jpg',
+    body: '<span aria-hidden="true"><b>Client:</b> <a href="https://zuliatec.com/" target="_blank">ZuliaTec</a>. <b>Projects:</b> Proof of concept of E-learning Platform for USA-based company. <b>Tech:</b> #Angular.js #Bootstrap #NodeJS #Express #PostgreSQL</span>',
+  },
+  {
+    title: 'Full-Stack Software Developer',
+    subtitle: '2016',
+    body: '<span aria-hidden="true"><b>Client:</b> ModyDev LLC, Panama. <b>Projects:</b> <a href="http://podiumbuzz.com/" target="_blank">Podiumbuzz<a/>, <a href="https://www.instagram.com/cuchibox/" target="_blank">Cuchibox</a>. <b>Tech:</b> #Angular.js #Bootstrap #Docker #Django #PostgreSQL #React</span>',
+  },
+  {
+    title: 'Software Architect',
+    subtitle: '2015',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1285322191031214081/DBj-kSx-_400x400.jpg',
+    body: '<span aria-hidden="true"><b>Client:</b> Guardián del Alba S.A. PDVSA Industrial. <b>Tech:</b> #C++ #python #java #Alfresco</span>',
+  },
+  {
+    title: 'Functional Analyst',
+    subtitle: '2014',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1285322191031214081/DBj-kSx-_400x400.jpg',
+    body: '<span aria-hidden="true"><b>Client:</b> Guardián del Alba S.A. PDVSA Industrial. <b>Tech:</b> #VisualParadign #OpenProject #UML</span>',
+  },
+  {
+    title: 'Software Developer',
+    subtitle: '2013',
+    avatar:
+      'https://pbs.twimg.com/profile_images/1285322191031214081/DBj-kSx-_400x400.jpg',
+    body: '<span aria-hidden="true"><b>Client:</b> Guardián del Alba S.A. PDVSA Industrial.<b>Tech:</b> #Javascript #QooxDoo #Python #R #SQLAlquemy #PostgresSQL</span>',
+  },
+  {
+    title: 'Software Engineer',
+    subtitle: '2012',
+    body: '<span aria-hidden="true"><b>Client:</b> Department of Informatics at IPAS Estadal, Mérida. <b>Tech:</b> #PHP #JQuey #MySQL</span?',
+  },
+  {
+    title: 'Internships',
+    subtitle: '2011',
+    body: '<span aria-hidden="true"><b>Client:</b> Department of Informatics at CNE Regional, Mérida. <b>Tech:</b> #PHP #MySQL </span>',
+  },
+  {
+    title: 'Technical Support',
+    subtitle: '2008',
+    body: 'CELCIEC. University of the Andes.',
+  },
+];
+
+export default experiences;
---
+++ src/data/skills.ts
@@ -0,0 +1,52 @@
+const skills = [
+  {
+    name: 'Javascript',
+    level: 5,
+  },
+  {
+    name: 'Css',
+    level: 4.5,
+  },
+  {
+    name: 'HTML',
+    level: 4.5,
+  },
+  {
+    name: 'TypeScript',
+    level: 3.5,
+  },
+  {
+    name: 'NodeJs',
+    level: 4.5,
+  },
+  {
+    name: 'SailsJs',
+    level: 5,
+  },
+  {
+    name: 'VueJs',
+    level: 4,
+  },
+  {
+    name: 'Vuetify',
+    level: 4,
+  },
+  {
+    name: 'ReactJs',
+    level: 3,
+  },
+  {
+    name: 'SQL',
+    level: 4,
+  },
+  {
+    name: 'Git',
+    level: 5,
+  },
+  {
+    name: 'Scrum',
+    level: 4.5,
+  },
+];
+
+export default skills;
