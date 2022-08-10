47ecd0a71d8a62f981bc1a708ab3e413425e7676
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2022-08-10 09:37:44 -0400

    Adding gitignore, adding new quasar v2 with vite, the latest and greates

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
